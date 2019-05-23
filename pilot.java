00f23e42eeeddfe6d3ff3384f0b78d795e29e76e
makajeez <buhari.alhassan0@gmail.com>
2019-05-23 23:19:44 +0100

    test program

---
+++ pilot.java
@@ -0,0 +1,6 @@
+public class
+        public static void main(String args[]){
+                System.out.println("Hello world");
+                System.out.println("This is just a pilot program");
+        }
+}
